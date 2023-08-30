0a779c43972f02fc84a4f541e2c29109fa4d758f
kainy01 <kainykawai@gmail.com>
2023-08-30 10:53:18 +0300

    Functions type and passing params

--- src/intro.ts
+++ src/intro.ts
@@ -12,3 +12,19 @@ enum Size {
 //this enum auto knows the values of these it gonna increase it it self
 let mySize: Size = Size.Medium;
 console.log(mySize); //2
+
+//passing optional param 
+function calc(inc: number, tax?: number): number {
+  if ((tax || 2022) < 2022)
+    return inc * 2;
+  return inc * 3
+}
+//passing optional param as giving it a value
+function calc2(inc: number, tax=2022): number {
+  if (tax   < 2022)
+    return inc * 2;
+  return inc * 3
+}
+calc(10_100, 2022)
+calc(10_100)
+calc2(10_100)
