147b9a4f404b76bcb0bf1b92883db0f1bd19c69c
kainy01 <kainykawai@gmail.com>
2023-08-30 11:07:20 +0300

    Objects types and type Aliases

--- src/intro.ts
+++ src/intro.ts
@@ -20,11 +20,36 @@ function calc(inc: number, tax?: number): number {
   return inc * 3
 }
 //passing optional param as giving it a value
-function calc2(inc: number, tax=2022): number {
-  if (tax   < 2022)
+function calc2(inc: number, tax = 2022): number {
+  if (tax < 2022)
     return inc * 2;
   return inc * 3
 }
 calc(10_100, 2022)
 calc(10_100)
 calc2(10_100)
+
+
+//objects
+let employess: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void
+} = {
+  id: 1, name: "Zainab", retire: (date: Date) => {
+    console.log(date)
+  }
+}
+
+
+//objects DRY define a type to objects to make code shorter
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void
+}
+let EmployesShorter: Employee = {
+  id: 1, name: "Zainab", retire: (date: Date) => {
+    console.log(date)
+  }
+}
